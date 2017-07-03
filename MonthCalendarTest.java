6aa3cd4370aa9ca3ebefd57371f903b214d6e7bb
alexeylistopadov <alexey.listopadov@gmail.com>
2017-07-03 17:09:10 +0300

    Update MonthCalendarTest.java

--- MonthCalendarTest.java
+++ MonthCalendarTest.java
@@ -2,11 +2,10 @@ public class MonthCalendarTest {
 
     public static void main(String[] args) {
         try {
+            CalendarConsoleWriter calendarConsoleWriter = new CalendarConsoleWriter();
             if (args.length == 0) {
-                CalendarConsoleWriter calendarConsoleWriter = new CalendarConsoleWriter();
                 calendarConsoleWriter.writeCalendar(new MonthCalendar());
             } else {
-                CalendarConsoleWriter calendarConsoleWriter = new CalendarConsoleWriter();
                 calendarConsoleWriter.writeCalendar(new MonthCalendar(Integer.parseInt(args[0])));
             }
         } catch (Exception e) {
